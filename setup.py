dfef79a3da0ee6ea7b8b6c23a0922382579a4e50
RajDwivedi03 <rajdwivedi4814@gmail.com>
2025-03-12 21:29:11 +0530

    1ST

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+
+    requirements=[]
+    with open('requirements.txt') as file_obj:
+      requirements=file_obj.readlines()
+      requirements=[req.replace("/n","")for req in requirements]
+    
+      if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+
+    return requirements    
+setup(
+    name='ML Project 2',
+    version='1.0.0.0',
+    packages=find_packages(),
+    install_requires=get_requirements('requirement.txt'),
+    author='Raj Dwivedi',
+    author_email='rajdwivedi4814@gmail.com'
+    )
